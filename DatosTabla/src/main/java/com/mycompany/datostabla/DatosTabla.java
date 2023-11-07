d2c0c4865722b532cafe377864c4eaedae7a277a
dclamor <122384597+dclamor@users.noreply.github.com>
2023-11-07 21:44:24 +0100

    Add files via upload

---
+++ DatosTabla/src/main/java/com/mycompany/datostabla/DatosTabla.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.datostabla;
+
+/**
+ *
+ * @author at11DAM2
+ */
+public class DatosTabla {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Vehicula/src/main/java/com/mycompany/vehicula/Vehicula.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vehicula;
+
+/**
+ *
+ * @author at11DAM2
+ */
+public class Vehicula {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
